a048f7b900a4f51730492923f74f23c2b92b8f10
Stefan Froemken <froemken@gmail.com>
2025-03-31 21:29:57 +0200

    Add command to create a new event

---
+++ Classes/Information/EventInformation.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package stefanfroemken/ext-kickstarter.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace StefanFroemken\ExtKickstarter\Information;
+
+class EventInformation
+{
+    private const EVENT_PATH = 'Classes/Event/';
+
+    public function __construct(
+        private readonly ExtensionInformation $extensionInformation,
+        private readonly string $eventClassName,
+    ) {}
+
+    public function getExtensionInformation(): ExtensionInformation
+    {
+        return $this->extensionInformation;
+    }
+
+    public function getEventClassName(): string
+    {
+        return $this->eventClassName;
+    }
+
+    public function getEventFilename(): string
+    {
+        return $this->eventClassName . '.php';
+    }
+
+    public function getEventFilePath(): string
+    {
+        return $this->getEventPath() . $this->getEventFilename();
+    }
+
+    public function getEventPath(): string
+    {
+        return $this->extensionInformation->getExtensionPath() . self::EVENT_PATH;
+    }
+
+    public function getNamespace(): string
+    {
+        return $this->extensionInformation->getNamespacePrefix() . 'Event';
+    }
+}
