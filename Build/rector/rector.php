52b56c263cab3d9aabacc1da6f0d9c1401bbdb6f
lina.wolf <lwolf@w-commerce.de>
2025-07-22 15:20:37 +0200

    [FEATURE] Introduce Rector

---
+++ Build/rector/rector.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\DeadCode\Rector\StaticCall\RemoveParentCallWithoutParentRector;
+use Rector\PostRector\Rector\NameImportingPostRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\ValueObject\PhpVersion;
+use Ssch\TYPO3Rector\CodeQuality\General\ConvertImplicitVariablesToExplicitGlobalsRector;
+use Ssch\TYPO3Rector\CodeQuality\General\ExtEmConfRector;
+use Ssch\TYPO3Rector\Configuration\Typo3Option;
+use Ssch\TYPO3Rector\Set\Typo3LevelSetList;
+use Ssch\TYPO3Rector\Set\Typo3SetList;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/../../Classes',
+        __DIR__ . '/../../Configuration',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withPhpVersion(PhpVersion::PHP_82)
+    ->withSets([
+        Typo3SetList::CODE_QUALITY,
+        Typo3SetList::GENERAL,
+        Typo3LevelSetList::UP_TO_TYPO3_12,
+    ])
+    // To have a better analysis from PHPStan, we teach it here some more things
+    ->withPHPStanConfigs([Typo3Option::PHPSTAN_FOR_RECTOR_PATH])
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+        ConvertImplicitVariablesToExplicitGlobalsRector::class,
+    ])
+    ->withConfiguredRule(ExtEmConfRector::class, [
+        ExtEmConfRector::PHP_VERSION_CONSTRAINT => '8.2.0-8.3.99',
+        ExtEmConfRector::TYPO3_VERSION_CONSTRAINT => '12.4.0-12.4.99',
+        ExtEmConfRector::ADDITIONAL_VALUES_TO_BE_REMOVED => [],
+    ])
+    ->withImportNames(importShortClasses: false, removeUnusedImports: true)
+    // If you use importNames(), you should consider excluding some TYPO3 files.
+    ->withSkip([
+        RemoveParentCallWithoutParentRector::class,
+        '*Build/*', // Exclude any "Build" subdirectory
+        '*Resources/*', // Exclude any "Build" subdirectory
+        '*Model/*'
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        typeDeclarations: true,
+        privatization: true,
+        instanceOf: true,
+        earlyReturn: true,
+        strictBooleans: true,
+    )
+    ->withSkip([])
+;
