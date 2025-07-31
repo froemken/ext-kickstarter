c253d44e376c66573aa62a785c81558cd7058597
lina.wolf <lwolf@w-commerce.de>
2025-07-31 19:17:13 +0200

    Move composer name to Question implementation

---
+++ Classes/Command/Input/Validator/ComposerNameValidator.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package stefanfroemken/ext-kickstarter.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace StefanFroemken\ExtKickstarter\Command\Input\Validator;
+
+use StefanFroemken\ExtKickstarter\Command\Input\Question\ComposerNameQuestion;
+
+class ComposerNameValidator implements ValidatorInterface
+{
+    private const COMPOSER_NAME_REGEX = '#^[a-z0-9]([_.-]?[a-z0-9]+)*/[a-z0-9](([_.]|-{1,2})?[a-z0-9]+)*$#';
+
+    public function getArgumentName(): string
+    {
+        return ComposerNameQuestion::ARGUMENT_NAME;
+    }
+
+    public function __invoke(mixed $answer): string
+    {
+        if (!is_string($answer)) {
+            throw new \RuntimeException(
+                'Composer name must be a string',
+                1753959716,
+            );
+        }
+
+        if (preg_match(self::COMPOSER_NAME_REGEX, $answer) !== 1) {
+            throw new \RuntimeException(
+                'Invalid composer package name. Package name must follow a specific pattern (see: https://getcomposer.org/doc/04-schema.md#name)',
+                1753959716,
+            );
+        }
+
+        return $answer;
+    }
+}
