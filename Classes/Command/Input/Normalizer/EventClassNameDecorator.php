6cb56ab180b19d5feb5c014364df14464b8a75f7
lina.wolf <lwolf@w-commerce.de>
2025-08-08 14:38:54 +0200

    [TASK] EventClassName as Question

---
+++ Classes/Command/Input/Normalizer/EventClassNameDecorator.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package stefanfroemken/ext-kickstarter.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace StefanFroemken\ExtKickstarter\Command\Input\Normalizer;
+
+use StefanFroemken\ExtKickstarter\Traits\TryToCorrectClassNameTrait;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+#[AutoconfigureTag('ext-kickstarter.inputHandler.event-class')]
+class EventClassNameDecorator implements NormalizerInterface
+{
+    use TryToCorrectClassNameTrait;
+    public function __invoke(?string $userInput): string
+    {
+        if ($userInput === null || $userInput === '') {
+            return '';
+        }
+
+        return $this->tryToCorrectClassName($userInput, 'Event');
+    }
+}
