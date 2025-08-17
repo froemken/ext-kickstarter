2460cccca58abda11c86912599a4bd6d92e354c0
Lina Wolf <48202465+linawolf@users.noreply.github.com>
2025-08-17 11:55:08 +0200

    Update Classes/Information/Validatation/InformationValidationException.php
    Co-authored-by: Stefan Frömken <froemken@gmail.com>

--- Classes/Information/Validatation/InformationValidationException.php
+++ Classes/Information/Validatation/InformationValidationException.php
@@ -4,7 +4,6 @@ namespace FriendsOfTYPO3\Kickstarter\Information\Validatation;
 
 final class InformationValidationException extends \RuntimeException
 {
-
     public function __construct(private array $errors, int $code = 0)
     {
         parent::__construct('Validation failed', $code);
